08a6b2b95e12e54c1013132cbed826472129c796
kimwoojin123 <kimwoojin1903@gmail.com>
2023-12-29 17:49:16 +0900

    점과 이름 태그 분리해야함

--- kim/app/page.tsx
+++ kim/app/page.tsx
@@ -1,51 +1,78 @@
 'use client'
-
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import studentList from './data/students';
-import SideNav from './ui/sideNav';
-import Content from './ui/content';
-import ActionForm from './ui/form';
-
+import './globals.css';
 
 export default function Home() {
-
   const [selectedStudent, setSelectedStudent] = useState<any>(null);
   const [name, setName] = useState('');
   const [introduce, setIntroduce] = useState('');
   const [advantage, setAdvantage] = useState('');
-  
-  const handleClick = (student: any) => {
+  const [clickedName, setClickedName] = useState<string | null>(null);
+
+  const handleStudentClick = (student: any) => {
     setSelectedStudent(student);
     setName(student.name);
     setIntroduce(student.introduce);
     setAdvantage(student.advantage);
+    setClickedName(student.name); // 클릭한 이름 설정
   };
 
- const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const findStudent = studentList.find(
+    const foundStudent = studentList.find(
       (student) => student.name === name
     );
-    if (findStudent) {
-      setSelectedStudent(findStudent);
-      setName(findStudent.name);
-      setIntroduce(findStudent.introduce);
-      setAdvantage(findStudent.advantage);
+    if (foundStudent) {
+      setSelectedStudent(foundStudent);
+      setName(foundStudent.name);
+      setIntroduce(foundStudent.introduce);
+      setAdvantage(foundStudent.advantage);
+      setClickedName(foundStudent.name); // 이름을 입력한 경우 설정
     } else {
       setSelectedStudent(null);
       setIntroduce('');
       setAdvantage('');
-      alert('해당하는 학생이 없습니다.')
+      alert('해당하는 학생이 없습니다.');
     }
   };
 
   return (
     <div className="w-lvw h-lvh flex justify-center items-center">
-      <SideNav studentList={studentList} handleStudentClick={handleClick} />
+      <div>
+        <ul>
+          {studentList.map((student, index) => (
+            <li
+              className={`cursor-pointer ${clickedName === student.name ? 'active' : ''} h-10 w-56`}
+              key={index}
+              onClick={() => handleStudentClick(student)}
+            >
+              <span className={`${clickedName === student.name ? 'move-right' : ''}`}>
+                {selectedStudent && selectedStudent.name === student.name ? '· ' : ''}
+                {student.name}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
       <div className="h-3/4 border"></div>
       <div>
-      <Content selectedStudent={selectedStudent} introduce={introduce} advantage={advantage} />
-      <ActionForm setName={setName} handleFormSubmit={handleSubmit} />
+        <div>
+          <p>{selectedStudent ? selectedStudent.name : ''} 소개</p>
+          <div>{introduce}</div>
+        </div>
+        <div>
+          <p>{selectedStudent ? selectedStudent.name : ''} 장점</p>
+          <div>{advantage}</div>
+        </div>
+        <form className="flex flex-col" onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            placeholder="Write student's name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit">Show</button>
+        </form>
       </div>
     </div>
   );
